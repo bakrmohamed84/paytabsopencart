32e128942e534f58be1c33a5851e5c9db238681e
bakrmohamed84 <bakrmohamed84@users.noreply.github.com>
2021-05-27 11:17:44 +0200

    first commit

---
+++ catalog/language/en-gb/extension/payment/paytabs_strings.php
@@ -0,0 +1,13 @@
+<?php
+// Text
+$_['all_text_title']        = 'Online payments powered by PayTabs';
+$_['creditcard_text_title'] = 'PayTabs - CreditCard';
+$_['mada_text_title']       = 'PayTabs - Mada';
+$_['omannet_text_title']    = 'PayTabs - OmanNet';
+$_['amex_text_title']       = 'PayTabs - Amex';
+$_['knet_text_title']       = 'PayTabs - KnPay';
+$_['applepay_text_title']   = 'PayTabs - ApplePay';
+$_['atfawry_text_title']    = 'PayTabs - @Fawry';
+$_['sadad_text_title']      = 'PayTabs - Sadad';
+$_['stcpay_text_title']     = 'PayTabs - StcPay';
+$_['valu_text_title']       = 'PayTabs - valU';
